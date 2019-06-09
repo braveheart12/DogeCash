c751d25f88816529b559b3eb2d076aff4b8a811a
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-06-09 14:36:36 -0500

    Fix missing

--- src/protocol.h
+++ src/protocol.h
@@ -105,7 +105,7 @@ public:
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action)
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion)
     {
         if (ser_action.ForRead())
             Init();
