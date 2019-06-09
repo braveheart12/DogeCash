a9cf10d49ccc3bc67af6065eb09b85cab266e8bf
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-06-09 16:26:26 -0500

    Update bignum.h

--- src/libzerocoin/bignum.h
+++ src/libzerocoin/bignum.h
@@ -103,16 +103,16 @@ public:
     }
 
     template<typename Stream>
-    void Serialize(Stream& s, int nType=0, int nVersion=PROTOCOL_VERSION) const
+    void Serialize(Stream& s) const
     {
-        ::Serialize(s, getvch(), nType, nVersion);
+        ::Serialize(s, getvch());
     }
 
     template<typename Stream>
-    void Unserialize(Stream& s, int nType=0, int nVersion=PROTOCOL_VERSION)
+    void Unserialize(Stream& s)
     {
         std::vector<unsigned char> vch;
-        ::Unserialize(s, vch, nType, nVersion);
+        ::Unserialize(s, vch);
         setvch(vch);
     }
 
